3b3d099ea041030257621ac613196c5310ec65d3
vickydepasupil <victoria.depasupil@gmail.com>
2017-06-01 21:59:29 +0800

    second edit to settings.py file

--- loginpage/settings.py
+++ loginpage/settings.py
@@ -126,12 +126,14 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-
-    '/path/to/my_project/my_app/static/',
+    os.path.join(PROJECT_ROOT, 'static'),
 )
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+
+#     '/path/to/my_project/my_app/static/',
+# )
 
 MEDIA_URL = '/login/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'login')
