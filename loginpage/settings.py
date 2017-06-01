0904a0141c84d00debb8845fc83fa72476f7a764
vickydepasupil <victoria.depasupil@gmail.com>
2017-06-01 22:05:04 +0800

    edited settings.py file

--- loginpage/settings.py
+++ loginpage/settings.py
@@ -124,12 +124,11 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-STATIC_URL = '/static/'
 # STATICFILES_DIRS = (
 
 #     '/path/to/my_project/my_app/static/',
