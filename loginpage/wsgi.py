0572c704dd0af70ba3c5991f52d2585ec97d1fab
vickydepasupil <victoria.depasupil@gmail.com>
2017-06-01 23:59:04 +0800

    final commit

--- loginpage/wsgi.py
+++ loginpage/wsgi.py
@@ -10,10 +10,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
-        
-application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "loginpage.settings")
 
