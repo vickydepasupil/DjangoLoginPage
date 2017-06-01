04f71d2a2e6445bbf28585022039a05904df802c
vickydepasupil <victoria.depasupil@gmail.com>
2017-06-01 21:57:02 +0800

    edit settings.py file

--- loginpage/settings.py
+++ loginpage/settings.py
@@ -118,6 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
 
