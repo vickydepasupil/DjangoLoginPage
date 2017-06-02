9ce4ca734f2e46b16f994a7b51a6a5c264185bb7
vickydepasupil <victoria.depasupil@gmail.com>
2017-06-02 11:56:00 +0800

    edit

--- login/views.py
+++ login/views.py
@@ -18,7 +18,7 @@ def register_view(request):
 		user.set_password(form.cleaned_data.get("password"))
 		user.save()
 
-		ser = authenticate(username="username", password="password")
+		user = authenticate(username="username", password="password")
 		login(request, user)
 
 		context = {
